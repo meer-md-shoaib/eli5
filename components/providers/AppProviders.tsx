31e518d3aea57a227c7d8926fb1585a6f874ee2d
Meer Mohammed Shoaib <shoaibmeermd@gmail.com>
2026-06-21 19:13:31 +0530

    Remove authentication and login page

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -3,14 +3,11 @@
 import type { ReactNode } from "react";
 import { ThemeProvider } from "@/hooks/useTheme";
 import { ExplainerProvider } from "@/hooks/useExplainer";
-import { SessionProvider } from "./SessionProvider";
 
 export function AppProviders({ children }: { children: ReactNode }) {
   return (
-    <SessionProvider>
-      <ThemeProvider>
-        <ExplainerProvider>{children}</ExplainerProvider>
-      </ThemeProvider>
-    </SessionProvider>
+    <ThemeProvider>
+      <ExplainerProvider>{children}</ExplainerProvider>
+    </ThemeProvider>
   );
 }
